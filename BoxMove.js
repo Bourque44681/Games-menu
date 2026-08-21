2e908323c7609096b07da2f801c7a265fb76c99f
Bourque44681 <bourque44681@students.apsb.org>
2026-08-21 16:11:22 +0000

    Update

---
+++ BoxMove.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const box = document.getElementById("box");
+    const area = document.getElementById("gameSpot");
+    const scoreValue = document.getElementById("scoreValue");
+    const MissValue = document.getElementById("clickMIssValue");
+    let score = 0;
+    let miss = 0;
+
+    function MoveBox() {
+        const areaWidth = area.clientWidth;
+        const areaHeight = area.clientHeight;
+        const boxHeight = box.offsetHeight;
+        const boxWidth = box.offsetWidth;
+        const randomX = Math.floor(Math.random() * (areaWidth - boxWidth));
+        const randomY = Math.floor(Math.random() * (areaHeight - boxHeight));
+        box.style.left = `${randomX}px`;
+        box.style.top = `${randomY}px`;
+    }
+
+    document.addEventListener("click", function(event){
+        const target = event.target;
+
+        if (target == box) {
+            score++;
+            scoreValue.textContent = score;
+            MoveBox();
+        }
+        else{
+            miss++;
+            MissValue.textContent = miss;
+        }
+    })
+    
+});
