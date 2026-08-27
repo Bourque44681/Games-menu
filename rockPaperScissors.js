8fba3a438bc9104c55d0a50808a117938e7d4924
Bourque44681 <bourque44681@students.apsb.org>
2026-08-27 14:34:23 +0000

    Updating

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     const rockLink = "https://static.vecteezy.com/system/resources/previews/023/289/772/original/a-lifelike-rock-formation-inspired-by-nature-set-against-a-transparent-background-generative-ai-png.png";
     const paperLink = "https://static.vecteezy.com/system/resources/previews/018/742/946/original/white-ripped-piece-of-paper-isolated-on-transparent-background-file-png.png";
-    const scissorsLink = "https://pngimg.com/d/scissors_PNG28.png";
+    const scissorsLink = "https://static.vecteezy.com/system/resources/previews/048/059/122/large_2x/red-handled-scissors-isolated-on-transparent-background-free-png.png";
 
     const showCase1 = document.getElementById("showCase1");
     const showCase2 = document.getElementById("showCase2");
@@ -13,9 +13,22 @@ document.addEventListener("DOMContentLoaded", function(){
     const GameOn = false;
 
     function ShowCase(link) {
+        const num = Math.floor((Math.random() * 3) + 1)
+        const link2 = null;
+        if (num == 1) {
+            link2 = rockLink
+        }
+        else if (num == 2) {
+            link2 = paperLink
+        }
+        else {
+            link2 = scissorsLink
+        }
         showCase1.src = link
-    }
+        showCase2.src = link2
+        chooseMenu.style.visibility = "hidden"
 
+    }
 
     document.addEventListener("click", function(element){
         const target = element.target
@@ -23,6 +36,12 @@ document.addEventListener("DOMContentLoaded", function(){
         if (target) {
             if (target == rock) {
                 ShowCase(rockLink)
+            }
+            else if (target == paper) {
+                ShowCase(paperLink)
+            }
+            else {
+                ShowCase(scissorsLink)
             }
         }
     })
