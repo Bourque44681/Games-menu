9bd89d9a0edbef4d039572d6397423223c8dcd00
Bourque44681 <bourque44681@students.apsb.org>
2026-09-02 15:46:12 +0000

    Updating questions in test

--- Test/test.js
+++ Test/test.js
@@ -20,9 +20,26 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
     const stf = {
-        0: ["How many states are in the United States?", "50", "51", "52", "54"],
-        1: ["How many continents are there?", "7", "6", "8", "9"],
-        2: ["What is the largest ocean on Earth?", "Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean"],
+        0:["How many states are in the United States?", "50", "51", "52", "54"],
+        1:["How many continents are there?", "7", "6", "8", "9"],
+        2:["What is the largest ocean on Earth?", "Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean"],
+        3:["How many bones do sharks have?", "0", "140", "275", "137"],
+        4:["What state has the most borders with other states?", "Missouri and Tennessee", "Tennessee", "Missouri", "Illinois"],
+        5:["What is the closest  planet to the sun", "Mercury", "Earth", "Venus", "Mars"],
+        6:["Who of the following was NOT and founding father:", "Abraham Lincoln", "Alexander Hamilton", "Thomas Stone", "George Washington"],
+        7:["During World War II, who led the D-Day invasion?", " Dwight D. Eisenhower", "George S. Patton", "Bernard Montgomery", "Teddy Roosevelt"],
+        8:["What movie production company was the first to build a theme park?", "Walt Disney", "Universal Studios", "Hollywood", "Pixar"],
+        9:["Which is a island located in the Caribbean?", "The Bahamas", "The Philippines", "Guam", "Puerto Rico"],
+        10:["What is 2+2","4","22", "3", "2"],
+        11:["What is the fastest substance on Earth?", "Photons", "Neutrinos", "Ultra-high-energy cosmic ray particles", "water"],
+        12:["There are some people on a train, 14 got off, there are now 20 people still left on the train. How many people were orginally on the train", "34", "32", "40", "30"],
+        13:["What is the most popular sport in the world?", "Soccer", "Football", "Baseball", "Tennis"],
+        14:["What is the most visited country on Earth?", "France", "United States", "Paris", "United Kingdom"],
+        15:["After what event was the National Anthem written?", "British Bombardment of Fort McHenry", "Battle of the Marne", "Gallipoli Campaign", "Battle of Jutland"],
+        16:["What are the first 7 digits of pi?", "3.141592", "3.141792", "3.141593", "3.141527"],
+        17:["What is the most popular programming language in the United States?", "Python", "Java Script", "Java", "PHP"],
+        18:["Which of the following is a 3 dimentional shape?", "sphere", "square", "rectangle", "circle"],
+        19:["What is the most abundant metal in the world?", "Aluminum", "Iron", "Magnesium", "Calcium"]
     };
     let alreadChosen = []
 
@@ -75,23 +92,38 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 
 
+    function check(num) {
+        if (num === answer) {
+            Correct++;
+            window.open("akdlfkdasf.html")
+        }
+        else {
+            Incorrect++;
+        };
+    };
+
+
     But1.addEventListener("click", function(){
         if (CanAnswer) {
+            check(1)
             PickQuestion();
         };
     });
     But2.addEventListener("click", function(){
         if (CanAnswer) {
+            check(2)
             PickQuestion();
         };
     });
     But3.addEventListener("click", function(){
         if (CanAnswer) {
+            check(3)
             PickQuestion();
         };
     });
     But4.addEventListener("click", function(){
         if (CanAnswer) {
+            check(4)
             PickQuestion();
         };
     });
