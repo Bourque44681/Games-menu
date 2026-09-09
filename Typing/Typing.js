e20792309b246ec5daac1d0be5308e8d977a93eb
Bourque44681 <bourque44681@students.apsb.org>
2026-09-09 15:39:07 +0000

    Typing Saving

--- Typing/Typing.js
+++ Typing/Typing.js
@@ -4,6 +4,17 @@ document.addEventListener("DOMContentLoaded", function(){
     const Words = document.getElementById("Words");
     const timeValue = document.getElementById("timeValue")
 
+    const wpmVal = document.getElementById("wpmVal")
+    const AccuracyVal = document.getElementById("AccuracyVal")
+    const WordsTypedVal = document.getElementById("WordsTypedVal")
+    const CahractersTypedVal = document.getElementById("CahractersTypedVal")
+    const CharactersMisspelledVal = document.getElementById("CharactersMisspelledVal")
+
+    var WordsTyped = 0;
+    var CharactersTyped = 0;
+    var CharactersMisspelled = 0;
+    var WPM = 0;
+    var Accuracy = 0;
 
     var WordsArray = ["apples","good", "yellow","purple", "berries", "screen", "understanding", "because", "light", "panda", "cat", "dog", "right", "left", "upside", "shore", "shoe", "bear", "table", "string", "addition", "contradicting"];
 
@@ -14,19 +25,19 @@ document.addEventListener("DOMContentLoaded", function(){
     };
 
     let Timer = null;
-
+    let TimerStf = null;
     let TimerVal = 25;
 
     function StartTuner() {
-        let TimerStf = setInterval(() => {
-            TimerVal--
-            timeValue.textContent = TimerVal
+            TimerStf = setInterval(() => {
+            TimerVal--;
+            timeValue.textContent = TimerVal;
 
             if (TimerVal === 0) {
-                clearInterval(TimerStf)
-            }
+                clearInterval(TimerStf);
+            };
         }, 1000);
-    }
+    };
 
 
     function CreateList(amount) {
@@ -34,11 +45,11 @@ document.addEventListener("DOMContentLoaded", function(){
             const randomIndex = Math.floor(Math.random() * WordsArray.length);
             const randomWord = WordsArray[randomIndex];
             WordsList.push(randomWord);
-        }
-    }
+        };
+    };
 
-    StartTuner()
-    CreateList(60)
+    StartTuner();
+    CreateList(60);
     UpdateDispaly();
 
     document.addEventListener("keydown", (event) => {
@@ -48,15 +59,19 @@ document.addEventListener("DOMContentLoaded", function(){
             WordsList[0] = WordsList[0].substring(1);
         }
         else if (event.code === "Enter") {
-            WordsList = []
-            CreateList(60)
-        }
+            clearInterval(TimerStf);
+            StartTuner();
+            TimerVal = 25;
+            timeValue.textContent = TimerVal;
+            WordsList = [];
+            CreateList(60);
+        };
 
         if (WordsList[0] === "") {
             if (key === " ") {
                 WordsList.shift();
                 CreateList(1);
-            }            
+            };
         };
 
         UpdateDispaly();
